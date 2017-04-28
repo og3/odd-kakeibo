680301c599cddfec669623b2b3082b759f2c581b
noriyuki togashi <n-togashi@xxxx.local>
2017-04-28 17:30:41 +0900

    emailカラムを追加。 deviseの仕様上、email無しにuser登録をすることができなかったため、元に戻す。

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,10 @@ ActiveRecord::Schema.define(version: 20170428054102) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                   default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
     t.string   "image"
     t.string   "income"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
