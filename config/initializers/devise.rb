44d2a1c47dae1eaab4fa29e439007431f9e93684
noriyuki togashi <n-togashi@xxxx.local>
2017-04-28 16:59:38 +0900

    未ログインでアクセスしてきたときはサインアップページへリダイレクトするように設定

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,10 @@
+class CustomFailure < Devise::FailureApp
+  def redirect_url
+    # 未ログインでアクセスしてきたときはサインアップページへリダイレクト
+    new_user_registration_path
+  end
+end
+
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
