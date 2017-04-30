409222c94d764b5348f27fc2382a328d37d95c8e
noriyuki togashi <n-togashi@xxxx.local>
2017-04-30 16:20:39 +0900

    Treasurer_modelを作成

---
+++ app/models/treasurer.rb
@@ -0,0 +1,2 @@
+class Treasurer < ApplicationRecord
+end
---
+++ db/migrate/20170430071502_create_treasurers.rb
@@ -0,0 +1,11 @@
+class CreateTreasurers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :treasurers do |t|
+      t.string :price, null: false
+      t.string :comment, null: false
+      t.string :category, null: false
+      t.references :user, null: false
+      t.timestamps
+    end
+  end
+end
