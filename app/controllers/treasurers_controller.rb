ae4f24e8f10dfd2f9d5cdb6d110f547cb5775383
noriyuki togashi <n-togashi@xxxx.local>
2017-04-30 16:30:06 +0900

    Treasurers_controllerにアクションを追加

--- app/controllers/treasurers_controller.rb
+++ app/controllers/treasurers_controller.rb
@@ -1,2 +1,19 @@
 class TreasurersController < ApplicationController
+
+# 会計の一覧を表示
+  def index
+  end
+
+# 会計を保存する
+  def create
+  end
+
+# 会計を編集する
+  def edit
+  end
+
+# 会計を削除する
+  def destroy
+  end
+
 end
