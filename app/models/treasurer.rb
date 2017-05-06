f2742dc0fbfa19901e2e8784db270505dbd7835b
noriyuki togashi <n-togashi@xxxx.local>
2017-05-06 18:47:31 +0900

    remote: trueでajaxを使用するための設定を追加

--- app/models/treasurer.rb
+++ app/models/treasurer.rb
@@ -1,4 +1,6 @@
 class Treasurer < ApplicationRecord
   belongs_to :user
-  
+    def for_js
+    {date: date, price: price, comment: comment, category: category, datetime: created_at.strftime(' %I:%M %p')}
+  end
 end
